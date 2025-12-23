449f1af9961fd6001ff8e72ccce898d9f285f745
Yuvraj17143 <yuvrajsingh62958@gmail.com>
2025-12-23 20:21:37 +0530

    Update socket.js

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -2,7 +2,9 @@ import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_API_BASE_URL, {
   transports: ["websocket"],
-  autoConnect: false
+  withCredentials: true,
+  autoConnect: false,
 });
+
 
 export default socket;
